9070d798548db15bee71736065fe8a805122f790
alejandro.mesias <alejandromesiasu@gmail.com>
2018-02-08 16:51:57 -0500

    functional test for input field

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,7 +15,23 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+
+        header_text = self.browser.find_element_by_tag_name('h1').header_text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        input_placeholder = inputbox.get_attribute('placeholder')
+        self.assertEqual(input_placeholder,'Enter a to-do item')
+
+        #enter a new to do
+        inputbox.send_keys('Buy this crazy thing')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy this crazy thing' for row in rows))
+
 
 if __name__ == '__main__':
     unittest.main(warnings = 'ignore')
